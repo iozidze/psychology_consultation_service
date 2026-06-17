b25a8c0248ca97f9abdc48e0e3266082cd0679db
Анна <anna.eshkereee@gmail.com>
2026-06-17 22:05:40 +0300

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,8 @@ from typing import Literal  # 👈 добавьте этот импорт
 
 class Settings(BaseSettings):
     port: int = 8000
-    cors_origins: str = "http://localhost:5173"
+    # cors_origins: str = "http://localhost:5173"
+    cors_origins: str = "https://psychology-consultation-service.onrender.com"
 
     database_url: str = "postgresql://postgres1:1d7diUVaHy5F5wW2iyrE4tB5DqUp78UH@dpg-d8oroa0k1i2s73ekuidg-a/psychology_qxo6"
 
@@ -15,7 +16,6 @@ class Settings(BaseSettings):
     db_connect_retries: int = 30
     db_connect_retry_delay: int = 2
     
-    # 🔥 Добавьте это поле:
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
 
     model_config = SettingsConfigDict(
